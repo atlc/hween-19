5b9fc12bace2eec53e3f3db237cef176ca76c5e2
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-10-05 19:07:59 -0500

    Reverted lights back to lights only (no motors), added in extra function

--- TopOfGameLights/TopOfGameLights.ino
+++ TopOfGameLights/TopOfGameLights.ino
@@ -1,8 +1,5 @@
 #include <Adafruit_NeoPixel.h>
-#include <Servo.h>
 
-#define R_BUTT_PIN A0
-#define SERVO_PIN 6
 #define NEO_PIN 7
 #define N_LEDS 60
 
@@ -11,44 +8,29 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, NEO_PIN, NEO_GRB + NEO_KHZ80
 // #define WHITE strip.Color(225,192,80)
 #define WHITE strip.Color(22,19,8)
 
-Servo servo;
-int servoCounter = 0;
-int rightButtState = 0;
-
 void setup() {
   Serial.begin(9600);
   strip.begin();
-  servo.attach(SERVO_PIN);
-  pinMode(R_BUTT_PIN, INPUT);
   chase();
   flash();
 }
 
 void loop() {
-  rightButtState = digitalRead(R_BUTT_PIN);
-  
-  if (rightButtState == HIGH) {
-    servoCounter++;
-    Serial.print("Button hit:     ");
-    Serial.println(servoCounter);
-    delay(250); 
-  }
-  
-  if (servoCounter == 0) {
-    servo.write(0);
-  } else if (servoCounter == 1) {
-    servo.write(60);
-  } else if (servoCounter == 2) {
-    servo.write(120);
-  } else if (servoCounter == 3) {
-    servo.write(179);
-  } else {
-    servoCounter = 0;
-  }
-//  rainbow(128);
+  chase();
+  flash();
+  flash();
+  rainbow(128);
+  secondsToStayFullyLit(30);
 }
 
-
+static void secondsToStayFullyLit(int s) {
+  int timeToStayLit = s * 1000;
+  for(int i=0; i<N_LEDS+8; i++) {
+    strip.setPixelColor(i, WHITE);
+    strip.show();
+  }
+  delay(timeToStayLit);
+}
 
 static void chase() {
   for(int i=0; i<N_LEDS+8; i++) {
@@ -76,7 +58,6 @@ static void flash() {
   delay(128);
 }
 
-/*
 void rainbow(uint8_t wait) {
   uint16_t i, j;
 
@@ -105,4 +86,4 @@ uint32_t Wheel(byte WheelPos) {
     return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
-*/
+
