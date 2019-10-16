9d14093b5089bb5653e24ed43c7ed8b1c8824766
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-10-15 20:57:48 -0500

    Added in initial round of LED matrix and speaker logic for WAM

---
+++ WhackAMole/WhackAMole.ino
@@ -0,0 +1,179 @@
+#include "LedControl.h"
+#include "binary.h"
+
+/*
+ DIN connects to pin 12
+ CLK connects to pin 11
+ CS connects to pin 10 
+*/
+LedControl lc=LedControl(12,11,10,1);
+
+// delay time between faces
+unsigned long delaytime=1000;
+
+// happy face
+byte hf[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
+// neutral face
+byte nf[8]={B00111100, B01000010,B10100101,B10000001,B10111101,B10000001,B01000010,B00111100};
+// sad face
+byte sf[8]= {B00111100,B01000010,B10100101,B10000001,B10011001,B10100101,B01000010,B00111100};
+
+
+#define B0  31
+#define C1  33
+#define CS1 35
+#define D1  37
+#define DS1 39
+#define E1  41
+#define F1  44
+#define FS1 46
+#define G1  49
+#define GS1 52
+#define aA1  55
+#define AS1 58
+#define B1  62
+#define C2  65
+#define CS2 69
+#define D2  73
+#define DS2 78
+#define E2  82
+#define F2  87
+#define FS2 93
+#define G2  98
+#define GS2 104
+#define aA2  110
+#define AS2 117
+#define B2  123
+#define C3  131
+#define CS3 139
+#define D3  147
+#define DS3 156
+#define E3  165
+#define F3  175
+#define FS3 185
+#define G3  196
+#define GS3 208
+#define aA3  220
+#define AS3 233
+#define B3  247
+#define C4  262
+#define CS4 277
+#define D4  294
+#define DS4 311
+#define E4  330
+#define F4  349
+#define FS4 370
+#define G4  392
+#define GS4 415
+#define aA4  440
+#define AS4 466
+#define B4  494
+#define C5  523
+#define CS5 554
+#define D5  587
+#define DS5 622
+#define E5  659
+#define F5  698
+#define FS5 740
+#define G5  784
+#define GS5 831
+#define aA5  880
+#define AS5 932
+#define B5  988
+#define C6  1047
+#define CS6 1109
+#define D6  1175
+#define DS6 1245
+#define E6  1319
+#define F6  1397
+#define FS6 1480
+#define G6  1568
+#define GS6 1661
+#define aA6  1760
+#define AS6 1865
+#define B6  1976
+#define C7  2093
+#define CS7 2217
+#define D7  2349
+#define DS7 2489
+#define E7  2637
+#define F7  2794
+#define FS7 2960
+#define G7  3136
+#define GS7 3322
+#define aA7  3520
+#define AS7 3729
+#define B7  3951
+#define C8  4186
+#define CS8 4435
+#define D8  4699
+#define DS8 4978
+
+
+// notes in the melody:
+int toxic[] = {
+  E3, G3, E4, D4, B3, B3, aA3, aA3, G3, D4, E4, B3, E4, B3, E4, E4, B3, E4, FS4, E4, D4, D4, B3, aA3, G3, D4, E3, G3, B3, aA3, G3, G3, E3, G3, B3, aA3, G3, aA3, B3, E3, G3, B3, aA3, G3, G3, E3, G3, B3, aA3, G3, aA3, E3
+};
+
+int notesInToxic = 52;
+
+void setup() {
+  lc.shutdown(0,false);
+  // Set brightness to a medium value
+  lc.setIntensity(0,8);
+  // Clear the display
+  lc.clearDisplay(0);       
+}
+
+void playToxic() {   
+  for (int thisNote = 0; thisNote < notesInToxic; thisNote++) {
+    int noteDuration = 1000 / 8;
+    tone(13, toxic[thisNote], noteDuration);
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(13);
+  }
+}
+
+void drawFaces(){
+  // Display sad face
+  lc.setRow(0,0,sf[0]);
+  lc.setRow(0,1,sf[1]);
+  lc.setRow(0,2,sf[2]);
+  lc.setRow(0,3,sf[3]);
+  lc.setRow(0,4,sf[4]);
+  lc.setRow(0,5,sf[5]);
+  lc.setRow(0,6,sf[6]);
+  lc.setRow(0,7,sf[7]);
+  delay(delaytime);
+  
+  // Display neutral face
+  lc.setRow(0,0,nf[0]);
+  lc.setRow(0,1,nf[1]);
+  lc.setRow(0,2,nf[2]);
+  lc.setRow(0,3,nf[3]);
+  lc.setRow(0,4,nf[4]);
+  lc.setRow(0,5,nf[5]);
+  lc.setRow(0,6,nf[6]);
+  lc.setRow(0,7,nf[7]);
+  delay(delaytime);
+  
+  // Display happy face
+  lc.setRow(0,0,hf[0]);
+  lc.setRow(0,1,hf[1]);
+  lc.setRow(0,2,hf[2]);
+  lc.setRow(0,3,hf[3]);
+  lc.setRow(0,4,hf[4]);
+  lc.setRow(0,5,hf[5]);
+  lc.setRow(0,6,hf[6]);
+  lc.setRow(0,7,hf[7]);
+  delay(delaytime);
+}
+
+void loop(){
+  playToxic();
+  drawFaces();
+}
